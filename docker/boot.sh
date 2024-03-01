a30b4e8515f5dbcd8a4063b64e0027c2ef5396d9
GevorgyanSam <SamvelGevorgyanPC@gmail.com>
2024-03-02 01:19:57 +0400

    Added docker-compose.yml

--- docker/boot.sh
+++ docker/boot.sh
@@ -7,16 +7,16 @@ add-apt-repository ppa:ondrej/php
 
 apt-get install -y composer php php-curl php-cli php-json php-zip php-mbstring php-xml php-mysql php-imagick php-gd
 
-cd /var/www/Framework
+cd /var/www/app
 cp .env.example .env
 composer install
 
 service apache2 start
 
-cp /var/www/Framework/docker/framework.conf /etc/apache2/sites-available/
+cp /var/www/app/docker/app.conf /etc/apache2/sites-available/
 cd /etc/apache2/sites-available/
 a2dissite 000-default.conf
-a2ensite framework.conf
+a2ensite app.conf
 service apache2 reload
 a2enmod rewrite
 service apache2 restart
